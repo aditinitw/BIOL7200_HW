3535ac9178e4c8c80cc42bd101a1ef3552bb5c10
Aditi Sinha <sinhaadt6@gmail.com>
2025-09-06 03:47:33 -0400

    update script to display number again

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,8 +4,10 @@ Query="$1"
 Subject="$2"
 Outfile="$3"
 
-makeblastdb -in "$Subject" -dbtype nucl -out dbfile
-tblastn -query "$Query" -db dbfile -outfmt "6 qseqid sseqid pident length qlen" > qn4blast.tsv
-awk '$3 > 30 && $4 / $5 > 0.9' qn4blast.tsv > "$Outfile"
+tblastn -query "$Query" -subject "$Subject" -outfmt "6 qseqid sseqid pident length qlen" > qn4blast.tsv
+
+awk '$3 > 30 && ($4 / $5) > 0.9' qn4blast.tsv > "$Outfile"
+
 matches=$(wc -l < "$Outfile")
 echo "$matches"
+
