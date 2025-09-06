d3072bf544597e56cca3a44bf44a0be4d33726ff
Aditi Sinha <sinhaadt6@gmail.com>
2025-09-06 03:41:10 -0400

    update script to display number

--- find_homologs.sh
+++ find_homologs.sh
@@ -7,4 +7,5 @@ Outfile="$3"
 makeblastdb -in "$Subject" -dbtype nucl -out dbfile
 tblastn -query "$Query" -db dbfile -outfmt "6 qseqid sseqid pident length qlen" > qn4blast.tsv
 awk '$3 > 30 && $4 / $5 > 0.9' qn4blast.tsv > "$Outfile"
-wc -l < "$Outfile"
+matches=$(wc -l < "$Outfile")
+echo "$matches"
